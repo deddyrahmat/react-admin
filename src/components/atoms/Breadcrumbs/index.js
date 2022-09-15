0039299b98be8143d7ddf3ef7dbc0d331d7c2f4a
deddyrahmat <deddyrahmat97@gmail.com>
2022-09-15 23:40:52 +0700

    update link breadcrumb

--- src/components/atoms/Breadcrumbs/index.js
+++ src/components/atoms/Breadcrumbs/index.js
@@ -1,27 +1,34 @@
-import { useState , Fragment} from 'react';
+import { useState, Fragment } from "react";
+import { Link } from "react-router-dom";
 
-import './Breadcrumb.css'
+import "./Breadcrumb.css";
 
-function Breadcrumbs({data}) {
+function Breadcrumbs({ data }) {
     //   state untuk simpan total data breadcrumbs
-  const [count, setCount] = useState(data?.length - 1);
+    const [count, setCount] = useState(data?.length - 1);
 
-  // jika jumlah item itu sama dengan count berarti itu adalah list breadcrumb yang aktif
-  return (
-    <nav>
-        <ol className="breadcrumb">
-        {data.map((item, index) => (
-          <Fragment key={index}>
-           {index !== count ? (
-            <li className="breadcrumb-item"><a href={item.url}>{item.title}</a></li>
-           ) : (
-            <li className="breadcrumb-item active">{item.title}</li>
-           )}
-          </Fragment>
-        ))}
-        </ol> 
-    </nav>
-  )
+    // jika jumlah item itu sama dengan count berarti itu adalah list breadcrumb yang aktif
+    return (
+        <nav>
+            <ol className="breadcrumb">
+                {data.map((item, index) => (
+                    <Fragment key={index}>
+                        {index !== count ? (
+                            <li className="breadcrumb-item">
+                                <Link to={item.url}>
+                                    <a>{item.title}</a>
+                                </Link>
+                            </li>
+                        ) : (
+                            <li className="breadcrumb-item active">
+                                {item.title}
+                            </li>
+                        )}
+                    </Fragment>
+                ))}
+            </ol>
+        </nav>
+    );
 }
 
-export default Breadcrumbs
+export default Breadcrumbs;
